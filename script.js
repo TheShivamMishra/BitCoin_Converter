ddfd57fac294f2e7da2ea30e61a90f190598027e
Shivam Mishra <36327761+dudeshivam@users.noreply.github.com>
2020-10-22 23:43:58 +0530

    Added CSS.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 //jshint esversion: 6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();   //new instantce of express;
 
 app.use(bodyParser.urlencoded({extended:true}));  //body parser is used to parse the body of json;
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(3000,function(){      //seting up the server to listen on port 3000;
     console.log("The Serve is Running at port 3000");
